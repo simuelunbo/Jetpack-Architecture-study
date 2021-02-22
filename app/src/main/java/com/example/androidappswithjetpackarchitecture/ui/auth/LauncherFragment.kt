14fbcbb86cf019e549a2660df7be90c31d9aec64
simuelunbo <simuelunbo@gmail.com>
2021-02-23 02:12:02 +0900

    Add gitIgnore and Navigation Graphs for Authentication

---
+++ app/src/main/java/com/example/androidappswithjetpackarchitecture/ui/auth/LauncherFragment.kt
@@ -0,0 +1,57 @@
+package com.example.androidappswithjetpackarchitecture.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.androidappswithjetpackarchitecture.R
+
+class LauncherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val register = view.findViewById<TextView>(R.id.register)
+        register.setOnClickListener {
+            navRegistration()
+        }
+        val login = view.findViewById<TextView>(R.id.login)
+        login.setOnClickListener {
+            navLogin()
+        }
+
+        val forgotPassword = view.findViewById<TextView>(R.id.forgot_password)
+        forgotPassword.setOnClickListener {
+            navForgotPassword()
+        }
+
+        val focusableView = view.findViewById<View>(R.id.focusable_view)
+        focusableView.requestFocus()
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+
+    }
+
+    private fun navRegistration() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+
+}
